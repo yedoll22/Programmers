87c331e7aae6eae3ece2f5bc9eb338d168d57f5a
yedoll22 <84077490+yedoll22@users.noreply.github.com>
2021-07-26 21:50:23 +0900

    반복문사용
    반복문(for)를 사용하여 배열의 평균값을 구하기

--- javascript/level1/평균구하기/solution.js
+++ javascript/level1/평균구하기/solution.js
@@ -1,8 +1,10 @@
 function solution(arr) {
     var answer = 0;
+    
     for (let i = 0; i < arr.length; i++) {
         answer = answer + arr[i];
     }
+    
     answer = answer / arr.length;
     return answer;
 }
